c7eaeb34406d17d9dcf1071ed0e19e37fd5021a4
diamanthaxhimusa <diamanthaxhimusa@gmail.com>
2017-02-20 11:05:14 +0100

    trends/chart page now has service selection

--- js/i18n.js
+++ js/i18n.js
@@ -6,8 +6,12 @@ var urlLangParam = decodeURIComponent((new RegExp('[?|&]lang=' + '([^&;]+?)(&|#|
 if(urlLangParam === 'sr'){
     lang = 'SR';
 }else if(urlLangParam === 'en'){
-    lang = 'EN'
-}else{
+    lang = 'EN';
+}
+// else if(urlLangParam === 'tr'){
+//     lang = 'TR';
+// }
+else{
     lang = 'AL';
 }
 
@@ -15,126 +19,150 @@ var i18n = {
     title:{
         AL: 'Platforma e OPI-t',
         SR: 'Platforma OPI',
-        EN: 'OPI Platform'
+        EN: 'OPI Platform',
+        TR: 'OPI Platformu'
     },
     description:{
         AL: 'Platforma e OPI-t sh\xEBrben p\xEBr t\xEB reflektuar rezultatet e grumbulluara nga opinionet e qytetar\xEBve n\xEB koh\xEB reale dhe n\xEB m\xEBnyr\xEB transparente, si dhe mund\xEBson p\xEBrmir\xEBsimin e politikave publike p\xEBr sh\xEBrbimet administrative q\xEB ofrohen nga institucionet e Republik\xEBs s\xEB Kosov\xEBs.',
         SR: 'Platforma OPI slu&zcaron;i za odra&zcaron;avanje rezultata prikupljenih od mi&scaron;ljenja gra&#273;ana u realno vreme i na transparentan na&ccaron;in, i omogu&cacute;ava pobolj&scaron;anje javnih politika za administrativne usluga koje se pru&zcaron;aju od strane institucija Republike Kosova.',
-        EN: 'OPI platform serves to reflect the results collected from the opinions of citizens in a timely and transparent manner, and enables the improvement of public policies for administrative services provided by the institutions of the Republic of Kosovo.'
+        EN: 'OPI platform serves to reflect the results collected from the opinions of citizens in a timely and transparent manner, and enables the improvement of public policies for administrative services provided by the institutions of the Republic of Kosovo.',
+        TR: 'OPI platformu, vatandaşların görüşleri doğrultusunda toplanan sonuçların zamanında ve şeffaf bir şekilde yansımasını sağlamakta ve Kosova Cumhuriyeti kurumlarının sağladığı idari hizmetler için kamu politikalarının geliştirilmesini sağlamaktadır.'
     },
     descriptionbottom:{
         AL: 'Projekti \xEBsht\xEB bashk\xEBfinancuar nga Ministria e Administrat\xEBs Publike (MAP) dhe Programi i Kombeve t\xEB Bashkuara p\xEBr Zhvillim (UNDP) n\xEB Kosov\xEB.',
         SR: 'Projekat je ko-finansiran od strane Ministarstva za javnu upravu (MJU) i Programa za razvoj Ujedinjenin Nacija (UNDP) na Kosovu.',
-        EN: 'The project is co-financed by the Ministry of Public Administration (MPA) and United Nations Development Programme (UNDP) in Kosovo.'
+        EN: 'The project is co-financed by the Ministry of Public Administration (MPA) and United Nations Development Programme (UNDP) in Kosovo.',
+        TR: ''
     },
     institutionLabel:{
         AL: "Institucioni",
         SR: "Institucije",
-        EN: "Institution"
+        EN: "Institution",
+        TR: 'Kurum'
     },
     serviceLabel:{
         AL: "Sh\xEBrbimi",
         SR: "Usluge",
-        EN: "Service"
+        EN: "Service",
+        TR: 'Hizmet'
     },
     logotext1:{
         AL: 'Republika e Kosov\xEBs',
         SR: 'Republika Kosova',
-        EN: 'Republic of Kosovo'
+        EN: 'Republic of Kosovo',
+        TR: 'Kosova Cumhuriyeti'
     },
     logotext2:{
         AL: 'Ministria&nbsp;e&nbsp;Administrat\xEBs&nbsp;Publike',
         SR: 'Ministarstvo&nbsp;Javne&nbsp;Uprave',
-        EN: 'Ministry&nbsp;of&nbsp;Public&nbsp;Administration'
+        EN: 'Ministry&nbsp;of&nbsp;Public&nbsp;Administration',
+        TR: 'Kamu&nbsp;Yönetimi&nbsp;Bakanlığı'
     },
     institutions:{
         AL: 'Institucione',
         SR: 'Institucija',
-        EN: 'Institutions'
+        EN: 'Institutions',
+        TR: 'Kurumlar'
     },
     services:{
         AL: 'Sh\xEBrbime',
         SR: 'Usluga',
-        EN: 'Services'
+        EN: 'Services',
+        TR: 'Hizmetler'
     },
     answers:{
         AL: 'qytetar\xEB',
         SR: 'gra&#273;ana',
-        EN: 'citizens'
+        EN: 'citizens',
+        TR: 'vatandaşlar'
     },
     satisfied:{
         AL: 'T\xEB k\xEBnaqur',
         SR: 'Zadovoljan',
-        EN: 'Satisfied'
+        EN: 'Satisfied',
+        TR: 'Memnuniyet'
     },
     moderatelySatisfied:{
         AL: 'Mesatarisht t\xEB k\xEBnaqur',
         SR: 'Umereno zadovoljan',
-        EN: 'Moderately Satisfied'
+        EN: 'Moderately Satisfied',
+        TR: 'Kısmi memnuniyet'
     },
     dissatisfied:{
         AL: 'T\xEB pak\xEBnaqur',
         SR: 'Nezadovoljan',
-        EN: 'Dissatisfied'
+        EN: 'Dissatisfied',
+        TR: 'Memnuniyetsizlik'
     },
     indicators:{
         quality:{
             AL: 'cil\xEBsin\xEB e<br>sh\xEBrbimit',
             SR: 'kvalitet pruzene<br>usluge',
-            EN: 'quality of<br>service rendered'
+            EN: 'quality of<br>service rendered',
+            TR: 'hizmet kalitesi'
         },
         staff:{
             AL: 'mir\xEBsjelljen e<br>zyrtarit',
             SR: 'ponasanje<br>sluzbenika',
-            EN: 'behaviour of<br>the official'
+            EN: 'behaviour of<br>the official',
+            TR: 'yetkili davranışı'
         },
         timeliness:{
             AL: 'koh\xEBzgjatjen e<br>sh\xEBrbimit',
             SR: 'pravovremenost<br>pruzene usluge',
-            EN: 'timeliness of<br>service provided'
+            EN: 'timeliness of<br>service provided',
+            TR: 'hizmet süresi'
         },
         payment:{
             AL: 'pages\xEBn e<br>sh\xEBrbimit',
             SR: 'isplatni nivo<br>pruzene usluge',
-            EN: 'payment level of<br>the rendered service'
+            EN: 'payment level of<br>the rendered service',
+            TR: 'hizmetin ödeme seviyesi'
         },
         online:{
             AL: 'meq\xEB sh\xEBrbimi nuk<br>\xEBsht\xEB online',
             SR: 'nema online<br>usluge',
-            EN: 'no online<br>service rendered'
+            EN: 'no online<br>service rendered',
+            TR: 'çevirim dışı'
         }
     },
     ranking:{
         AL: 'Niveli vl\xEBresues',
         SR: 'Rang',
-        EN: 'Ranking'
+        EN: 'Ranking',
+        TR: 'Klasman'
     },
     trends:{
-        AL:'Trendi',
+        AL:'Trendet',
         SR:'unknown',
-        EN:'Trends'
+        EN:'Trends',
+        TR: 'Eğilimler'
     },
     visualizer:{
         AL: 'Vizualizim',
         SR: 'Vizualizator',
-        EN: 'Visualizer'
+        EN: 'Visualizer',
+        TR: 'Gösterge'
     },
     serviceList:{
         show: {
             AL: 'trego sh\xEBrbimet',
             SR: 'poka&#382;i usluge',
-            EN: 'show services'
+            EN: 'show services',
+            TR: 'hizmetleri göster'
         },
         hide:{
             AL: 'fsheh sh\xEBrbimet',
             SR: 'sakrij usluge',
-            EN: 'hide services'
+            EN: 'hide services',
+            TR: 'hizmetleri gizle'
         }
     },
     searchFor:{
         AL: 'Search for...',
         SR: 'Search for...',
-        EN: 'Search for...'
+        EN: 'Search for...',
+        TR: 'Için ara ...'
     }
 
 };
@@ -147,9 +175,11 @@ $(function() {
 
     }else if(urlLangParam === 'en'){
         $('.selected-language').html('English');
-
-
-    }else{
+    }
+    // else if(urlLangParam === 'tr'){
+    //     $('.selected-language').html('T&uuml;rk&ccedil;e');
+    // }
+    else{
         $('.selected-language').html('Shqip');
     }
 });
